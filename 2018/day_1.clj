b61e2faac99c8cdf4b2e419f8cf14c12197798cc
Florent Pastor <florent.pastor@ostmodern.co.uk>
2018-12-01 23:36:07 +0100

    fixup! 2018 - Day 1

--- 2018/day_1.clj
+++ 2018/day_1.clj
@@ -6,7 +6,7 @@
 (defn resulting_frequency [drifts]
   (reduce + drifts))
 
-(println (resulting_frequency (parse_drifts (slurp "day_1_part_1_input")))) ;; First problem
+(println (resulting_frequency (parse_drifts (slurp "day_1_input")))) ;; First problem
 
 (defn find_duplicate_frequency [frequencies last_frequency drifts]
   (let [last_frequency (+ last_frequency (first drifts))]
@@ -14,4 +14,4 @@
       last_frequency
       (recur (conj frequencies last_frequency) last_frequency (rest drifts)))))
 
-(println (find_duplicate_frequency #{0} 0 (cycle (parse_drifts (slurp "day_1_part_1_input"))))) ;; Second problem
+(println (find_duplicate_frequency #{0} 0 (cycle (parse_drifts (slurp "day_1_input"))))) ;; Second problem
